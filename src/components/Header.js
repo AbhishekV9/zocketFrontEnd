1b3ee5deeb7285e1370136ae1ab6bc3f42d5b17b
Abhsihske Verma <avishekv444@gmail.com>
2021-11-21 00:13:48 +0530

    some changes in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import logo from "../assets/Group 12428.png"
+
+import logo from "../assets/Group 12428.png";
+import home from "../assets/Home.png";
+import about from "../assets/About.png";
+import blog from "../assets/Blog.png";
+import jobs from "../assets/Jobs.png";
+import faq from "../assets/FAQ’s.png";
+import contact from "../assets/Contact.png";
+import vector from "../assets/Vector 3.png";
+import selected from "../assets/Rectangle 3547.png";
 
 const Header = () => {
     return (
         <div>
             <img className="logo" src={logo} alt="zocket-logo" />
-            <p id="home">Home</p>
-            <p id="about" className="notSelected">About</p>
-            <p id="blogs" className="notSelected">Blog</p>
-            <p id="jobs" className="notSelected">Jobs</p>
-            <p id="faq" className="notSelected">FAQ's</p>
-            <p id="contact" className="notSelected">Contact</p>
-            <p className="down"></p>
-            <p className="selected"></p>
+            <img className="nav" src={home} alt="home" style={{ left:"850px" }} />
+            <img className="nav" src={about} alt="about" style={{ left:"923px" }} />
+            <img className="nav" src={blog} alt="blog" style={{ left:"996px" }} />
+            <img className="nav" src={jobs} alt="jobs" style={{ left:"1059px" }} />
+            <img className="nav" src={faq} alt="faq" style={{ left:"1122px" }} />
+            <img className="nav" src={contact} alt="contact" style={{ left:"1193px" }} />
+            <img className="bar" src={vector} alt="vector" style={{ left:"81px" }} />
+            <img className="bar" src={selected} alt="selected" style={{ left:"841px" }} />
         </div>
     )
 }
