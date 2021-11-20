ed4054527c47af949b5eeca08f6e0c20f3ded50b
Abhsihske Verma <avishekv444@gmail.com>
2021-11-21 02:22:18 +0530

    sending request to the server

--- src/components/main_components/Register.js
+++ src/components/main_components/Register.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 import DontMiss from "../../assets/Group 12459.png";
 import Men from "../../assets/Group 16750.png";
 import Ads from "../../assets/Facebook Ads In 30 Seconds.png"
@@ -9,6 +10,31 @@ import Vector2 from "../../assets/Vector-2.png";
 import Vector1 from "../../assets/Vector-1.png";
 
 const Register = () => {
+
+    const [Email, setEmail] = useState("");
+
+    const handlechange=(e)=>{
+        setEmail(e.target.value);
+    }
+
+    const handleRegister=async ()=>{
+        let formBody=[];
+        let encodedKey=encodeURIComponent("email");
+        let encodedValue=encodeURIComponent(Email);
+        formBody.push(encodedKey+'=' + encodedValue);
+        
+        const url="http://localhost:8000/register";
+        await fetch(url,{
+            method:'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body:formBody
+        }).then(res => res.json())
+        .then((data)=>{
+            console.log(data);
+        });
+        setEmail("");
+    }
+
     return (
         <div>
             <img className="dont"src={DontMiss} alt="Don't Miss"></img>
@@ -16,8 +42,8 @@ const Register = () => {
             <img src={Ads} className="ads" alt="heading"/>
             <img src={Vector} className="vector" alt="vector"/>
             <img src={Grow} className="grow" alt="Grow Your Buisness"/>
-            <input type="email" placeholder="&nbsp;&nbsp;&nbsp;Your Email ID" className="ip"/>
-            <img src={Group} className="b" alt="button"/>
+            <input type="email" value={Email} placeholder="&nbsp;&nbsp;&nbsp;Your Email ID" className="ip" onChange={handlechange}/>
+            <img src={Group} className="b" alt="button" onClick={ handleRegister}/>
             <img src={Vector2} className="vector2" alt="arrow" />
             <img src={Vector1} className="vector1" alt="circle" />
         </div>
