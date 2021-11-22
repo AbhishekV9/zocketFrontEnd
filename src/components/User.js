3584b0f148cf26c952a279775ced38530f017257
Abhsihske Verma <avishekv444@gmail.com>
2021-11-22 17:12:25 +0530

    showing alert in user info

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Alert from '@mui/material/Alert';
 import { useEffect,useState } from 'react';
 import Header from './Header';
 
@@ -12,11 +13,18 @@ const User_info = (props) => {
         .then(res => res.json())
         .then((data)=>{
             setUser(data.user);
+        }).catch((err)=>{
+            console.log("hi")
+            
+            console.log(err);
         })
     },[path])
 
     return (        
         <div>
+            <div className="alert-user">
+                <Alert severity={"success"}> A Notification Email is Sent To {user.email}</Alert>
+            </div>
             <Header/>
             <h1 className="userInfo1">Email:-</h1>
             <h1 className="userEmail">{user.email}</h1>
